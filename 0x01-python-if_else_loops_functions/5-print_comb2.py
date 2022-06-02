bdf7655096ea57681408c2c2d1d08618ad5df83b
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-06-02 16:05:35 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for number in range(100):
-    if (number != 99):
-        print("{}{}, ".format(int(number / 10), number % 10), end="")
-    else:
-        print("{}{}".format(int(number / 10), number % 10))
+for i in range(0, 100):
+    print('{:02d}'.format(i), ', ' if i < 99 else '\n', sep='', end='')
